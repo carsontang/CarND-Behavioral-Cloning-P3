a30945ba6261d0c208fe38c5bfdc90bf602d4de1
Carson Tang <ctang@MacBook-Pro-102.local>
2017-08-30 07:23:34 -0700

    augment data with memory optimized solution

--- loader/data.py
+++ loader/data.py
@@ -16,23 +16,27 @@ def load_data(log, img_dir):
                 current_path = os.path.join(img_dir, filename)
                 image = cv2.imread(current_path)
                 images.append(image)
+                images.append(cv2.flip(image, 1))
             steering_center = float(line[3])
             correction = 0.2
             steering_left = steering_center + correction
             steering_right = steering_center - correction
             measurements.append(steering_center)
+            measurements.append(-steering_center)
             measurements.append(steering_left)
+            measurements.append(-steering_left)
             measurements.append(steering_right)
+            measurements.append(-steering_right)
 
     # augment data
-    augmented_images, augmented_measurements = [], []
-    for image, measurement in zip(images, measurements):
-        augmented_images.append(image)
-        augmented_measurements.append(measurement)
-        augmented_images.append(cv2.flip(image, 1))
-        augmented_measurements.append(measurement * -1.0)
+    # augmented_images, augmented_measurements = [], []
+    # for image, measurement in zip(images, measurements):
+    #     augmented_images.append(image)
+    #     augmented_measurements.append(measurement)
+    #     augmented_images.append(cv2.flip(image, 1))
+    #     augmented_measurements.append(measurement * -1.0)
 
-    X_train = np.array(augmented_images)
-    y_train = np.array(augmented_measurements)
+    X_train = np.array(images)
+    y_train = np.array(measurements)
 
     return X_train, y_train
