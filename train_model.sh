b7045367a0e1f5ab3985f9a7944407cf87a118db
Carson Tang <tang.carson@gmail.com>
2017-08-29 22:41:58 -0700

    script to train model

---
+++ train_model.sh
@@ -0,0 +1,9 @@
+python model.py \
+	-lr 0.0001 \
+	-l /home/carson/Documents/CarND-Behavioral-Cloning-P3/driving_log.csv \
+	-i /home/carson/Documents/CarND-Behavioral-Cloning-P3/IMG \
+	--epochs 20 \
+	--droprate 0.5 \
+	--activation 'relu' \
+	--conv1filters 24 \
+	--conv2filters 32
