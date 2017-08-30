8c77e68638eee5c92595928afabe6214872270b7
Carson Tang <ctang@MacBook-Pro-102.local>
2017-08-29 23:01:00 -0700

    load in center, left, right camera images

--- loader/data.py
+++ loader/data.py
@@ -9,13 +9,20 @@ def load_data(log, img_dir):
     with open(os.path.join(log)) as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
-            source_path = line[0]
-            filename = source_path.split('/')[-1]
-            current_path = os.path.join(img_dir, filename)
-            image = cv2.imread(current_path)
-            images.append(image)
-            measurement = float(line[3])
-            measurements.append(measurement)
+            # load center, left, right camera images
+            for i in range(3):
+                image_path = line[i]
+                filename = image_path.split('/')[-1]
+                current_path = os.path.join(img_dir, filename)
+                image = cv2.imread(current_path)
+                images.append(image)
+            steering_center = float(line[3])
+            correction = 0.2
+            steering_left = steering_center + correction
+            steering_right = steering_center - correction
+            measurements.append(steering_center)
+            measurements.append(steering_left)
+            measurements.append(steering_right)
     X_train = np.array(images)
     y_train = np.array(measurements)
 
